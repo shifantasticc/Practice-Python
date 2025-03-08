f57f2acf587214e2a16209ae6e27f0827923ceeb
shifantasticc <shifastudyit@gmail.com>
2025-03-08 20:14:12 +0530

    Adding basic Python Program

---
+++ basic.py
@@ -0,0 +1,21 @@
+print("Hello World!!")
+
+#sum
+x,y = 5,8
+print(x+y)
+
+#data types
+a = 5 #int
+b = 7.4 #float
+c = "S" #string
+d = 3+8j #complex
+
+l1 = [10, "Shifa", 30+7j, 40.8] #List
+t1 = "A", 33, 30+5j, 7.5 #Tuple
+s1 = {44, "rope", 8+9j, 5.8} #Set
+d1 = []
+print("List: ",l1)
+print("Tuple: ",t1)
+print("Set: ",s1)
+print("Dict: ",d1)
+
