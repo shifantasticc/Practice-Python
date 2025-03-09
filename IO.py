9f7fd6917258504810ba7fc3f319a8c2d0ba8bfc
shifantasticc <shifastudyit@gmail.com>
2025-03-09 19:47:02 +0530

    Adding basic I/O

---
+++ IO.py
@@ -0,0 +1,19 @@
+#printing KW list
+import keyword
+print(keyword.kwlist)
+
+#creating a screen
+'''import turtle
+z = turtle.Screen()
+z.setup(200, 200)'''
+
+#sum of 2 numbers
+x = int(input("Enter Num 1:"))
+y = int(input("Enter Num 2:"))
+print("Addition of 2 number is:", x+y)
+
+#area of a circle
+r = int(input("Enter radius:"))
+pi = 3.14
+print("Area of a circle is:", pi*r*r)
+
