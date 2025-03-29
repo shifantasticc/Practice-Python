7ce54e22eb60e3eae12315d9d737e63d1d91dc45
shifantasticc <shifastudyit@gmail.com>
2025-03-29 21:30:09 +0530

    Adding while loop Program

---
+++ PythonAssigment/WhileLoop.py
@@ -0,0 +1,14 @@
+# Q.3
+word = 'orange'
+i = 0
+
+while i < len(word):
+    print('Current Letter:', word[i])
+    i += 1
+print()
+
+i = 0
+while i < len(word):
+    print('Current Letter Length:', len(word[i]))
+    i += 1
+print()
