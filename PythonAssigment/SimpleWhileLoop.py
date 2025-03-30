76fbca772fa35a8d9a1178b3b2fa007d6f8cbf30
shifantasticc <shifastudyit@gmail.com>
2025-03-30 22:49:22 +0530

    Adding simple while loop Program

---
+++ PythonAssigment/SimpleWhileLoop.py
@@ -0,0 +1,21 @@
+# Q.1
+i = 0
+while i < 6:
+    print(i)
+    i += 1
+print()
+
+fruits = ['banana', 'apple', 'mango', 'grapes']
+i = 0
+
+# Q.2
+while i < len(fruits):
+    print('Current fruit at', i, "is", fruits[i])
+    i += 1
+
+print("Good Bye!!!")
+print()
+
+
+
+
