3a9551ebef320291e357a1a2a5dd95543eb950e3
shifantasticc <shifastudyit@gmail.com>
2025-04-01 20:05:38 +0530

    Adding pattern while loop Program

---
+++ PythonAssigment/PatternWhileLoop.py
@@ -0,0 +1,20 @@
+# Q.5
+i = 1
+while i < 6:
+    j = 1
+    while j < 6:
+        print(i, end="")
+        j += 1
+    print()
+    i += 1
+print()
+
+# Q.6
+i = 1
+while i < 6:
+    j = 1
+    while j < 6:
+        print(j, end="")
+        j += 1
+    print()
+    i += 1
