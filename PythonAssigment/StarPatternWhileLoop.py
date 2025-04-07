62bd93ac5e6024501c0ae9b3b94fe96c76eba817
shifantasticc <shifastudyit@gmail.com>
2025-04-07 22:20:29 +0530

    Adding Star Pattern in While Loop

---
+++ PythonAssigment/StarPatternWhileLoop.py
@@ -0,0 +1,23 @@
+# Q.7
+i = 1
+while i < 6:
+    j = 1
+    while j < 6:
+        if j <= i:
+            print(i, end="")
+        j += 1
+    print()
+    i += 1
+print()
+
+# Q.8
+i = 1
+while i <= 5:
+    j = 1
+    while j <= 5:
+        if j <= i:
+            print(j, end="")
+        j += 1
+    print()
+    i += 1
+print()
